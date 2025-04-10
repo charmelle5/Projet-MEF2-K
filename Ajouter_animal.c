5bbabbf3fde91f5c771aeb8b7ebda8b38407d386
Lasugaa <lasugaa.k@gmail.com>
2025-04-10 02:45:19 +0200

    Ajouter_animal.c

--- Ajouter_animal.c
+++ Ajouter_animal.c
@@ -1,1 +1,16 @@
-
+void Ajouter_animal(Animal* chenil, int taille){
+  if(chenil == NULL|| taille<0){
+    printf("Erreur !");
+    exit(1);
+  }
+  if(taille==50){
+    printf("Le chenil est plein, nous ne pouvons pas acceuillir un nouveau animal ");
+  }
+  else{
+    Animal* Newchenil = malloc((taille+1)*sizeof(Animal));
+    for (int i = 0; i < taille; i++)
+    {
+        Newchenil[i] = chenil[i];
+    }
+    Newchenil[taille+1] = creation_animaux();
+    Newchenil[taille+1].num = rand()%100;
