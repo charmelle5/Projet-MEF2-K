57d771199fdbca6cbbcbd34f06cd98ac9b85209c
Lasugaa <lasugaa.k@gmail.com>
2025-04-10 02:45:07 +0200

    main.c

--- main.c
+++ main.c
@@ -1,15 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<Animal.h>
+#define MAXCHAR 50
 
-int n;
-while(n>50 || n<0){
-  printf("Saisir le nombre d'animaux dans le chenil :\n");
-  scanf("%d",&n);
-}
+int main() {
+  int n;
+  while(n>50 || n<0){
+    printf("Saisir le nombre d'animaux dans le chenil :\n");
+    scanf("%d",&n);
+  }
 
-Animal* chenil = malloc(n*sizeof(Animal));
+  Animal* chenil = malloc(n*sizeof(Animal));
 
-for (int i=0; i<n; i++){
-  creation_animaux();
+  for (int i=0; i<n; i++){
+    creation_animaux();
+  }
+  int numero;
+  printf("Quelle action voulez-vous réaliser ? 1.Rechercher un/des animaux\n 2.Ajouter un animal qui vous a été confié \n3.Adoption d’un animal");
+  scanf("%d",&numero);
+
+  switch (numero)
+    {
+    case 1:
+        return recherche_animaux();
+    case 2:
+        return Ajouter_animal();
+    case 3:
+        return Adoption_animal();
+    default:
+        return 
+          printf("Quelle action voulez-vous réaliser ? 1.Rechercher un/des animaux\n 2.Ajouter un animal qui vous a été confié \n3.Adoption d’un animal");
+          scanf("%d",&numero);
+    }
+  
 }
