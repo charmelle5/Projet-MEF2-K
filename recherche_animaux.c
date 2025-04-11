bf1a386ff4548dcd72fceff27198ff55a4c09812
nirmine001 <nirminebouya@gmail.com>
2025-04-11 17:12:16 +0200

    Recherche_animaux.c

--- recherche_animaux.c
+++ recherche_animaux.c
@@ -7,7 +7,7 @@ char* nom;
 int espece;
 printf("Taper le nom de l'animal :" \n)
 scanf("%s,nom);
-printf("Taper l'espèce de l'animal :" \n) 
+printf("Taper l'espèce de l'animal : 1:Chien\n 2:Chat\n 3:Hamster\n 4:Autruche\n 5:Lapin\n 6:Poisson\n");
 scanf("%d,espece);
 printf("Taper l'age de l'animal :" \n)
 scanf("%d,nom);
