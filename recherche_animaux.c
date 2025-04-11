6d658c1f3335a09708eafe0d56e5e7e0e6195fec
nirmine001 <nirminebouya@gmail.com>
2025-04-11 17:10:12 +0200

    recherche_animaux.c

--- recherche_animaux.c
+++ recherche_animaux.c
@@ -1,8 +1,17 @@
-void Rechercher_animal(Animal* chenil1, int taille){
-   if(chenil == NULL|| taille<0){
+void Rechercher_animal(Animal* chenil1, int SIZE){
+   if(chenil == NULL){
     printf("Erreur !");
     exit(1);
   }
-char* nom;
+char* nom; 
+int espece;
 printf("Taper le nom de l'animal :" \n)
-scanf("%s, 
+scanf("%s,nom);
+printf("Taper l'espèce de l'animal :" \n) 
+scanf("%d,espece);
+printf("Taper l'age de l'animal :" \n)
+scanf("%d,nom);
+for(int i=0; i<SIZE;i++){
+   if( strcmp(nom)!=strcmp(chenil1[i].nom){
+   }
+
