7833fb5d148cf7edaa2dd4af08833584b5ad0f4e
Lasugaa <lasugaa.k@gmail.com>
2025-04-11 16:24:14 +0200

    Creation_animaux.c

--- Creation_animaux.c
+++ Creation_animaux.c
@@ -5,7 +5,13 @@ void creation_animaux(){
   printf("Saisir le nom de l'animal :\n");
   scanf("%s",a.nom);
   printf("Saisir l'espece de l'animal :\n");
-  scanf("%d",&a.espece); // pas sur//
+  printf("Saisir une numéro : 1:Chien\n 2:Chat\n 3:Hamster\n 4:Autruche\n 5:Lapin\n 6:Poisson\n");
+  scanf("%d",&a.espece); 
+  if(a.espece<1 || a.espece>6){
+    printf("Erreur");
+    printf("Saisir une numéro : 1:Chien\n 2:Chat\n 3:Hamster\n 4:Autruche\n 5:Lapin\n 6:Poisson\n");
+    scanf("%d",&a.espece);
+  }
   printf("Saisir l'annee de naissance :\n");
   scanf("%d",&a.annee);
   printf("Saisir le poids de l'animal :\n");
