a1580c8eb8ab571b0e7a3b9f15eb3a8e124a873a
nirmine001 <nirminebouya@gmail.com>
2025-04-11 16:20:47 +0200

    recherche_animaux.c

--- recherche_animaux.c
+++ recherche_animaux.c
@@ -1,2 +1,8 @@
 void Rechercher_animal(Animal* chenil1, int taille){
-  
+   if(chenil == NULL|| taille<0){
+    printf("Erreur !");
+    exit(1);
+  }
+char* nom;
+printf("Taper le nom de l'animal :" \n)
+scanf("%s, 
