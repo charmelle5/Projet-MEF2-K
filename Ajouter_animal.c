515e7dd74532722f8dded7815b5b88320cfe3a4d
Lasugaa <lasugaa.k@gmail.com>
2025-04-23 19:33:25 +0200

    Update Ajouter_animal.c

--- Ajouter_animal.c
+++ Ajouter_animal.c
@@ -1,20 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void Ajouter_animal(Animal* chenil, int taille){
-  if(chenil == NULL|| taille<0){
+void Ajouter_animal(Animal** chenil, int* taille){
+  if (*taille >= MAX_ANIMAUX) {
+        printf("Le chenil est plein (capacité max: %d).\n", MAX_ANIMAUX);
+        return;
+  }
+
+  Animal nouvel_animal = creation_animal();
+
+  if(chenil == NULL){
     printf("Erreur !");
     exit(1);
   }
-  if(taille==50){
-    printf("Le chenil est plein, nous ne pouvons pas acceuillir un nouvel animal ");
+  // Réallocation mémoire pour agrandir le tableau
+  Animal* Newchenil = realloc(*chenil, (*taille + 1) * sizeof(Animal));
+  if (Newchenil == NULL) {
+        printf("Erreur mémoire !\n");
+        return;
   }
-  else{
-    Animal* Newchenil = malloc((taille+1)*sizeof(Animal));
-    for (int i = 0; i < taille; i++)
-    {
-        Newchenil[i] = chenil[i];
-    }
-    Newchenil[taille+1] = creation_animaux();
-    Newchenil[taille+1].num = rand()%100;
-  }
+  *chenil = Newchenil;
+  // Ajout de l'animal
+  (*chenil)[*taille] = nouvel_animal;
+  (*taille)++;
+
+  printf("\n✅ Animal ajouté !\n");
+}
