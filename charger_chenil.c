aab6155e37656f5a96245f1c9e02d543a549261e
Lasugaa <lasugaa.k@gmail.com>
2025-04-26 16:53:30 +0200

    Create charger_chenil.c

---
+++ charger_chenil.c
@@ -0,0 +1,12 @@
+void charger_chenil(Animal** chenil, int* taille) {
+    // Actualisation du tableau chenil en lisant le ficher //
+    FILE* fichier = fopen("Animaux_Chenil.dat", "rb");
+    if (fichier == NULL) {
+        return; // Fichier inexistant (normal au premier lancement)
+    }
+
+    fread(taille, sizeof(int), 1, fichier);
+    *chenil = malloc(*taille * sizeof(Animal));
+    fread(*chenil, sizeof(Animal), *taille, fichier);
+    fclose(fichier);
+}
