c8ca47b15845697bb95be2fc253cb2e7452c6975
Lasugaa <lasugaa.k@gmail.com>
2025-04-27 12:16:11 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -12,7 +12,7 @@ typedef struct{
 }Animal;
 
 // Prototypes //
-void creation_animal();
+Animal creation_animal();
 void ajouter_animal(Animal** chenil, int* taille);
 void rechercher_animal(Animal* chenil, int taille);
 void afficher_animal(Animal a);
