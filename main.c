abf9aab24f457f00e6844097b1df3795d001eedf
Lasugaa <lasugaa.k@gmail.com>
2025-04-27 12:31:11 +0200

    Update main.c

--- main.c
+++ main.c
@@ -35,36 +35,46 @@ FILE *fichier = fopen("fichier.txt", "w");  // "w" = écriture (crée/écrase)
     printf("Écriture terminée.\n");
 
     
-  Animal* chenil = NULL;
-  int taille = 0;
-  int choix;
-    do{
-      printf("\n=== MENU PRINCIPAL ===\n");
-      printf("1. Rechercher un/des animaux\n");
-      printf("2. Ajouter un animal\n");
-      printf("3. Adopter un animal\n");
-      printf("4. Quitter\n");
-      printf("Votre choix : ");
-      scanf("%d",&choix);
-      getchar(); // Pour consommer le '\n'
+ Animal* chenil = NULL;
+ int taille = 0;
+ int choix;
+ 
+  // Charger les animaux existants
+  charger_chenil(&chenil, &taille);
+    
+  do{
+    printf("\n=== MENU PRINCIPAL ===\n");
+    printf("1. Rechercher un/des animaux\n");
+    printf("2. Ajouter un animal\n");
+    printf("3. Adopter un animal\n");
+    printf("4. Quitter\n");
+    printf("Votre choix : ");
+    scanf("%d",&choix);
+    getchar(); // Pour consommer le '\n'
 
-      switch(choix) {
-            case 1:
-                rechercher_animal(chenil, taille);
-                break;
-            case 2:
-                ajouter_animal(&chenil, &taille);
-                break;
-            case 3:
-                adopter_animal(&chenil, &taille);
-                break;
-            case 4:
-                printf("Au revoir !\n");
-                break;
-            default:
-                printf("Choix invalide. Veuillez réessayer.\n");
-        }
-    } while(choix != 4);
+    switch(choix) {
+          case 1:
+              rechercher_animal(chenil, taille);
+              break;
+          case 2:
+              ajouter_animal(&chenil, &taille);
+              break;
+           case 3:
+              adopter_animal(&chenil, &taille);
+              break;
+          case 4:
+            printf("Au revoir !\n");
+               break;
+          default:
+              printf("Choix invalide. Veuillez réessayer.\n");
+    }
+  } while(choix != 4);
+
+  // Sauvegarder avant de quitter //
+  sauvegarder_chenil(chenil, taille);
+    
+  // Libérer la mémoire //
+  free(chenil);
   
-  
+  return 0;
 }
