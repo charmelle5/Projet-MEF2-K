7e282dc7b0dd002c1098f6ab0ab93620ed9c7f2c
Lasugaa <lasugaa.k@gmail.com>
2025-04-27 17:43:52 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -11,10 +11,18 @@ typedef struct{
   char commentaire[MAXCOM];
 }Animal;
 
-// Prototypes //
+// Structure pour stocker les temps de nettoyage
+typedef struct {
+    int temps_quotidien;
+    int temps_hebdomadaire;
+} TempsNettoyage;
+
+// Prototypes
 Animal creation_animal();
 void ajouter_animal(Animal** chenil, int* taille);
 void rechercher_animal(Animal* chenil, int taille);
 void afficher_animal(Animal a);
 void sauvegarder_chenil(Animal* chenil, int taille);
 void charger_chenil(Animal** chenil, int* taille);
+void calculer_temps_nettoyage(Animal* chenil, int taille);
+TempsNettoyage get_temps_nettoyage(Type espece);
