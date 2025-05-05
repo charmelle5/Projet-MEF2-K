48ddc348e486f48ea866e065f7624b494d9f17c0
charmelle5 <nanacharmelle77@gmail.com>
2025-05-05 23:56:50 +0200

    Update Creation_chenil.c

--- Creation_chenil.c
+++ Creation_chenil.c
@@ -1,1 +1,11 @@
-
+void afficher_animal(Animal a) {
+    const char* especes[] = {"", "Chien", "Chat", "Hamster", "Autruche", "Lapin", "Poisson"};
+    printf("\n N°%d ---\n", a.num);
+    printf("Nom: %s\n", a.nom);
+    printf("Espece: %s\n", especes[a.espece]);
+    printf("Annee naissance: %d\n", a.annee);
+    printf("Poids: %.2f kg\n", a.poids);
+    if (strlen(a.commentaire)){
+      printf("Commentaire: %s\n", a.commentaire);
+    }
+}
