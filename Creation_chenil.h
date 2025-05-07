faface96b0d3d5347d018316eb8297a75fe3a116
charmelle5 <nanacharmelle77@gmail.com>
2025-05-07 23:03:20 +0200

    Update Creation_chenil.h

--- Creation_chenil.h
+++ Creation_chenil.h
@@ -1,1 +1,34 @@
+#ifndef CREATION_CHENIL_H
+    #define CREATION_CHENIL_H
+
+        #include<stdio.h>
+        #include <stdlib.h>
+        #include <string.h>
+
+        #define MAXCHAR 50
+        #define MAXCOM 200
+        #define MAX_ANIMAUX 50
+
+
+        typedef enum{
+        CHIEN=1, CHAT, HAMSTER, AUTRUCHE, LAPIN, POISSON
+        }Type;
+
+        typedef struct{
+        int num;
+        char nom[MAXCHAR];
+        Type espece;
+        float poids;
+        int annee;
+        char commentaire[MAXCOM];
+        }Animal;
+
+        // Prototypes
+        Animal creation_animal();
+        void afficher_animal(Animal a);
+        void sauvegarder_chenil(Animal* chenil, int taille);
+        void charger_chenil(Animal** chenil, int* taille);
+        
+#endif
+
 
