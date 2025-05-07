8d75aac9f4cd415c06870f633ed4a7be6833d07d
charmelle5 <nanacharmelle77@gmail.com>
2025-05-07 23:04:33 +0200

    Create Option_menu.h

---
+++ Option_menu.h
@@ -0,0 +1,10 @@
+#ifndef OPTION_MENU_H
+    #define OPTION_MENU_H
+    #include"Creation_chenil.h"
+
+        Animal creation_animal();
+        void ajouter_animal(Animal** chenil, int* taille);
+        void rechercher_animal(Animal* chenil, int taille);
+        void adopter_animal(Animal** chenil, int* taille);
+
+#endif
