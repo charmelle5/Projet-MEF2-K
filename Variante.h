8cd50b23be4ec2e9517e73dd8d58b597f7f6d06f
charmelle5 <nanacharmelle77@gmail.com>
2025-05-07 23:05:31 +0200

    Create Variante.h

---
+++ Variante.h
@@ -0,0 +1,14 @@
+#ifndef VARIANTE_H
+    #define VARIANTE_H
+    #include"Creation_chenil.h"
+    
+        // Structure pour stocker les temps de nettoyage
+        typedef struct {
+        int temps_quotidien;
+        int temps_hebdomadaire;
+        } TempsNettoyage;
+    TempsNettoyage temps_nettoyage();
+    TempsNettoyage temps_nettoyage(Type espece);
+    void affiche_temps_nettoyage(Animal* chenil, int taille);
+
+#endif
